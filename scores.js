cdef720b7eb553436300eeff20fe2e4eba736cfa
Sai Manoj <152248557+saimanojsirigiri@users.noreply.github.com>
2024-07-22 06:41:56 +0530

    update data

--- scores.js
+++ scores.js
@@ -112,6 +112,20 @@ app.put("/updateData", async(req,res,next) => {
                 if(data) return res.status(200).json({message: "New Data Saved"})
                 else return res.status(400).json({message: "User not found"});
             }
+        }else{
+            // Insert new score data if it doesn't exist
+            const newScore = new scoresData({
+              event_id,
+              userName,
+              correctAnswersCount,
+              scores,
+              averageTimeToAnswer,
+              level
+            });
+      
+            const data = await scoresData.create(newScore);
+            if(data) return res.status(200).json({message: "New Data Saved"})
+            else return res.status(400).json({message: "User not found"});
         }
     }catch(err){
         return res.status(500).json({message: "Internal Server Error"});
